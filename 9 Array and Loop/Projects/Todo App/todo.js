8714e32e6e827b566a2a3b750bbb2c87c6c2ec1e
prashu-tech <prashantparsane894@gmail.com>
2024-12-12 17:11:53 +0530

    Added my repository to github

---
+++ 9 Array and Loop/Projects/Todo App/todo.js
@@ -0,0 +1,47 @@
+let todoList = [
+  {
+    item: 'Buy Milk',
+    dueDate: '4/10/2024'
+  },
+  {
+    item: 'GO to College', 
+    dueDate: '2/3/2024'
+  }
+];
+
+
+
+displayItem();
+
+function addTodo() {
+  let inputElement = document.querySelector('#todo-input');
+  let dateElement = document.querySelector('#todo-date');
+  let todoItem = inputElement.value; 
+  let todoDate = dateElement.value; 
+  todoList.push({item: todoItem, dueDate: todoDate});
+  inputElement.value = '';
+  dateElement.value = '';
+
+  displayItem();
+}
+
+function displayItem() {
+  let containerElement = document.querySelector('.todo-container');
+
+  let newHtml = '';
+
+  for (let i = 0; i < todoList.length; i++) {
+    // let item = todoList[i].item;
+    // let dueDate = todoList[i].dueDate;
+    let {item, dueDate} = todoList[i];
+    newHtml += `
+    <span>${item}</span>
+    <span>${dueDate}</span>
+    <button class="btn-delete" onclick="todoList.splice(${i}, 1); displayItem();">Delete</button>
+
+
+    `;
+
+  }
+  containerElement.innerHTML = newHtml;
+}
